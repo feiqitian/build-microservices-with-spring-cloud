5c4d468c0f27f6ca3c1343b58e0948944feff579
Jiahua Liu <jiahliu@thoughtworks.com>
2017-06-08 10:47:35 +0800

    initial project

---
+++ src/main/java/com/thoughtworks/korprulu/buildmicroserviceswithspringcloud/BuildMicroservicesWithSpringCloudApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.korprulu.buildmicroserviceswithspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildMicroservicesWithSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildMicroservicesWithSpringCloudApplication.class, args);
+	}
+}
